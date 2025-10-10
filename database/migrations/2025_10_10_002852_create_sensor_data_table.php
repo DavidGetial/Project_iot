0f485fcc8520ed3d790f034ba80e6d253cf683a9
David Getial <ginscreen2025@gmail.com>
2025-10-09 19:31:17 -0500

    migration

--- database/migrations/2025_10_10_002852_create_sensor_data_table.php
+++ database/migrations/2025_10_10_002852_create_sensor_data_table.php
@@ -36,6 +36,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('sensor_datas');
+        Schema::dropIfExists('sensor_data');
     }
 };
